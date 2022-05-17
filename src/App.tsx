060408d496dceadd758f13f44e395a00e5288d46
Debora <debora.pozzebon@gmail.com>
2022-05-16 23:40:00 -0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.png'
+import instagram from './assets/instagram.svg'
+import whatsapp from './assets/whatsapp.svg'
+import './app.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="principal">
+            <img src={logo} alt="imagem de logo" className='logo' />
+            <h1>Site em construção!</h1>
+
+            Enquanto ele não fica pronto, entre em contato e tire suas dúvidas pelo meu whatsapp ou instagram.
+
+            <div className='redes-sociais'>
+                <a href='https://www.instagram.com/grazielamatana/' target='_blank'>
+                    <img src={instagram} alt="imagem de logo" />
+                </a>
+
+                <a href='https://api.whatsapp.com/send?phone=54992032963'>
+                    <img src={whatsapp} alt="imagem de logo" />
+                </a>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
