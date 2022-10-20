924edbc18f77097bebe30d7df28e7b4336104c84
amandakehl <amandakehldefarias@hotmail.com>
2022-10-19 22:48:59 -0300

    correção de termos de uso e política de privacidade

--- src/components/TermsOfUse/index.tsx
+++ src/components/TermsOfUse/index.tsx
@@ -13,16 +13,27 @@ export default function TermsOfUseComponent() {
       <div className={styles.allTerms}>     
         <h2 className={styles.subtitle}>TERMOS DE USO:</h2>
         <span className={styles.term}><b>1)</b> Nenhum serviço jurídico é prestado neste site.</span>
+        <br />
         <span className={styles.term}><b>2)</b> Sou advogada especializada em direito societário e também falo de contratos e legal design. Aqui apenas divulgo informações que acredito ser importante e também uso o site como um canal de comunicação com pessoas interessadas no meu trabalho.</span>
+        <br />
         <span className={styles.term}><b>3)</b> Você não precisa pagar nada para usar o este site e usufruir do conteúdo que está aqui.</span>
+        <br />
         <span className={styles.term}><b>4)</b> O termo Usuário que tratamos aqui refere-se a qualquer pessoa que esteja navegando no site. </span>
+        <br />
         <span className={styles.term}><b>5)</b> Eu escrevo todos os artigos e materiais disponibilizados para ajudá-los, porém, não permito que você utilize esse material para qualquer outra finalidade que não seja aprender, ok? Caso tenha interesse em divulgar meus textos, entre em contato para que eu autorize expressamente.</span>
+        <br />
         <span className={styles.term}><b>6)</b> Tomei todas as medidas para que este site seja o mais seguro possível, mas mesmo assim recomendo que, antes de baixar qualquer conteúdo, você instale antivírus e programas de proteção.</span>
+        <br />
         <span className={styles.term}><b>7)</b> Os Termos de Uso poderão sofrer atualizações, as quais estarão disponíveis no site. Além disso, se eu realizar alguma ação que a lei exija sua autorização, você receberá um aviso antes para poder aceitar ou recusar.</span>
+        <br />
         <span className={styles.term}><b>8)</b> A titularidade e os direitos relativos ao site pertencem exclusivamente à Graziela Matana. O acesso e a utilização do site e dos conteúdos nele presentes não lhe confere qualquer direito ou prerrogativa sobre propriedade intelectual.</span>
+        <br />
         <span className={styles.term}><b>9)</b> Você já sabe, mas vale lembrar que todo o conteúdo do site, incluindo artigos, e-books, newsletters, programas, bases de dados, arquivos, textos, imagens, layouts, cabeçalhos, domínios e demais elementos, foram criados, desenvolvido ou cedidos ao controlador do site, sendo, portanto, de propriedade exclusiva GRAZIELA MATANA ADVOCACIA ou a ela licenciado e encontra-se protegido pelas leis brasileiras e tratados internacionais que versam sobre direitos de propriedade intelectual, em especial a Lei n. 9.610/98 (Lei de Direitos Autorais).</span>
+        <br />
         <span className={styles.term}><b>10)</b> O Usuário que violar as proibições contidas na legislação sobre propriedade intelectual e nestes Termos será responsabilizado, civil e criminalmente, pelas infrações cometidas, além de poder ser impedido de acessar o site.</span>
+        <br />
         <span className={styles.term}><b>11)</b> Você concorda que a legislação aplicável para fins destes Termos de Uso e das Políticas de Privacidade será a vigente na República Federativa do Brasil e, também que o foro da cidade Caxias do Sul, Rio Grande do Sul, Brasil, será o único competente para dirimir qualquer questão ou controvérsia oriunda ou resultante do uso do site.</span>
+        <br />
       </div>
     </div>
     </>
